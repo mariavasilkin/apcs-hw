2977bf2595ec024c81feecfbc6b8b04596fd63c3
unknown <m.vasilkin@gmail.com>
2014-03-16 22:40:53 -0400

    Not quicksort yet, but partition is working

--- 07-QuickSort/QuickSort2.java
+++ 07-QuickSort/QuickSort2.java
@@ -1,30 +1,30 @@
 import java.util.*;
 public class QuickSort2 {
 
-    public static int partition(int[]a) {
-	return (partition( a, 0, a.length-1));
+    public static void  partition(int[]a) {
+        partition( a, 0, a.length-1);
 	//For actual Quicksort:remove return statement
     }
-    public static int partition (int[] a, int L, int R) {
+    public static void partition (int[] a, int L, int R) {
 	int wall = 0;
+
 	//base cases:
 	if (a.length < 1) 
-	//For actual Quicksort:
-	    //	    return;
-	    return -1;
+
+	    return;
+	//return -1;
 	if (a.length == 1) 
-	//For actual Quicksort:
-	    //return;
-	    return 0;
+	    return;
+	//return 0;
 
 	//choose a random pivot:
-	int pivot = (int)(Math.random() * (a.length - 1));
+	int pivot = (int)(Math.random() * ((R-L) - 1));
 
 	//If both the left and right are at the pivot, return the pivot
 	if (L == pivot && R == pivot) 
 	//For actual Quicksort:
-	    //	    return;
-	    return pivot;
+	    return;
+	    //  return pivot;
 
 	//Swap the pivot with the rightmost value:
 	int c = a[R];
@@ -55,29 +55,32 @@ public class QuickSort2 {
 	a[wall] = a[pivot];
 	a[pivot] = f;
 
-	System.out.println("Value: " + a[pivot]);
 
-	System.out.print("Index: ");
-	return pivot;
+	//System.out.println("Value: " + a[pivot]);
+
+	//System.out.print("Index: ");
+	//return pivot;
 
 
 	//For actual Quicksort:
-	/*if (wall != 0) 
+	if (wall >= 2) 
 	    partition(a, 0, wall);
 	if (wall != a.length-1)
-	    partition(a, wall, a.length-1);
-	*/
+	    partition(a, wall, R);
+	return;
+	
     }
     
     public static void main( String[] args) {
 	int[] a = new int[]{5,1,76,10,4,100,2,50,36};
 
-	//For actual Quicksort: partition(a);
-	for (int i = 0; i < 10; i++) {   
+	//For actual Quicksort: 
+	partition(a);
+	/*	for (int i = 0; i < 10; i++) {   
 	    System.out.println(partition(a,0,a.length-1));
 	    System.out.println("\n");
-	}
+	    }*/
 	//For actual Quicksort:
-	//System.out.println(Arrays.toString(a));
+	System.out.println(Arrays.toString(a));
     }
 }
