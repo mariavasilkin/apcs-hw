e2b38401a20bcd8914b27f88212674fd9947a112
unknown <m.vasilkin@gmail.com>
2014-04-27 23:04:56 -0400

    HW 16 added

---
+++ 16-Trees/Driver.java
@@ -0,0 +1,14 @@
+public class Driver {
+       public static void main(String[] args) {
+       	      BST tree = new BST();
+	      tree.insert(10);
+	      tree.insert(4);
+	      tree.insert(15);
+	      tree.insert(2);
+	      tree.insert(7);
+	      tree.insert(12);
+	      tree.insert(18);
+	      tree.insert(5);
+	      System.out.println(root.getData());
+	 }
+}
